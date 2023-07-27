04a11b64d6e089e284148a74722c86855176bb3b
maxxprogramming <sendmailtomaxx@gmail.com>
2023-07-27 13:03:06 +0200

    fix repository

---
+++ ejerc007/src/main/java/es/cic/ejerc007/Repository/ButacaRepository.java
@@ -0,0 +1,14 @@
+package es.cic.ejerc007.Repository;
+
+import es.cic.ejerc007.Entity.Cine;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ButacaRepository extends CrudRepository<Cine, Long> {
+	
+	
+	 
+}
+
