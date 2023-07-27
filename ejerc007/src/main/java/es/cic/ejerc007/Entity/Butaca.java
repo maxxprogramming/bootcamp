fff83ed6a05d8b436cef14c23a6d92a8828eefd4
Jorge Balbontin <you@example.com>
2023-07-27 13:08:03 +0200

    Clase creada

--- ejerc007/src/main/java/es/cic/ejerc007/Entity/Butaca.java
+++ ejerc007/src/main/java/es/cic/ejerc007/Entity/Butaca.java
@@ -1,5 +1,41 @@
 package es.cic.ejerc007.Entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Butaca {
 
+	private Boolean comprada;
+	private Sala sala;
+	private Sesion sesion;
+	private double precio;
+	
+	
+	public Boolean getComprada() {
+		return comprada;
+	}
+	public void setComprada(Boolean comprada) {
+		this.comprada = comprada;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public Sesion getSesion() {
+		return sesion;
+	}
+	public void setSesion(Sesion sesion) {
+		this.sesion = sesion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
+	
 }
