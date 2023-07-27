c5d58f67f7aab9a528e37042d47aa3d785b6ebd9
Jorge Balbontin <you@example.com>
2023-07-27 13:01:59 +0200

    Clase creada

--- ejerc007/src/main/java/es/cic/ejerc007/Entity/Sesion.java
+++ ejerc007/src/main/java/es/cic/ejerc007/Entity/Sesion.java
@@ -2,4 +2,29 @@ package es.cic.ejerc007.Entity;
 
 public class Sesion {
 
+	private String pelicula;
+	private String horario;
+	private Sala sala;
+	
+	
+	public String getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(String pelicula) {
+		this.pelicula = pelicula;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	
+	
 }
