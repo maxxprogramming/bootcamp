0bf1d10c14d26025743d5660af52cc56f622ba7d
Jorge Balbontin <you@example.com>
2023-07-27 13:13:05 +0200

    Clase creada

--- ejerc007/src/main/java/es/cic/ejerc007/Entity/Ticket.java
+++ ejerc007/src/main/java/es/cic/ejerc007/Entity/Ticket.java
@@ -1,5 +1,50 @@
 package es.cic.ejerc007.Entity;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+
+@Entity
 public class Ticket {
 
+	private long id;
+	private List<Butaca> butacas;
+	private Sala sala;
+	private Sesion sesion;
+	private double precioTotal;
+	
+	public List<Butaca> getButacas() {
+		return butacas;
+	}
+
+	public void setButacas(List<Butaca> butacas) {
+		this.butacas = butacas;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Sesion getSesion() {
+		return sesion;
+	}
+
+	public void setSesion(Sesion sesion) {
+		this.sesion = sesion;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	
+	
 }
