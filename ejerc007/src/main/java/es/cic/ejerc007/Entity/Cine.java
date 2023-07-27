8439ca6da51f87b8f4deac8fb61681222a523dff
Your Name <you@example.com>
2023-07-27 12:39:52 +0200

    Clase Cine hecha

--- ejerc007/src/main/java/es/cic/ejerc007/Entity/Cine.java
+++ ejerc007/src/main/java/es/cic/ejerc007/Entity/Cine.java
@@ -5,4 +5,21 @@ import jakarta.persistence.Entity;
 @Entity
 public class Cine {
 
+	private Sala[] salas;
+
+	public Cine() {
+		super();
+		salas=new Sala[3];
+	}
+
+	public Sala[] getSalas() {
+		return salas;
+	}
+
+	public void setSalas(Sala[] salas) {
+		this.salas = salas;
+	}
+	
+	
+	
 }
