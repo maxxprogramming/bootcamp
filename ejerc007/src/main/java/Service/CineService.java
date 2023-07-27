2eda25bddd4bd9bbcc830228bf535b7826744a28
maxxprogramming <sendmailtomaxx@gmail.com>
2023-07-27 13:08:11 +0200

    esqueleto service

--- ejerc007/src/main/java/Service/CineService.java
+++ ejerc007/src/main/java/Service/CineService.java
@@ -5,9 +5,35 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.cic.ejerc007.Entity.Ticket;
+import es.cic.ejerc007.Repository.ButacaRepository;
+import es.cic.ejerc007.Repository.CineRepository;
+import es.cic.ejerc007.Repository.SalaRepository;
+import es.cic.ejerc007.Repository.SesionRepository;
+import es.cic.ejerc007.Repository.TicketRepository;
+
 
 @Service
 public class CineService {
+
+	
+@Autowired
+CineRepository repoCine;
+
+
+@Autowired
+ButacaRepository repoButaca;
+
+@Autowired
+SalaRepository repoSala;
+
+@Autowired
+SesionRepository repoSesion;
+
+@Autowired
+TicketRepository repoTicket;
+
+
 
 
 
